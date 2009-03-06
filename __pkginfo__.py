3c314a5e8a9219893c4fee2a0fee71a424eaedc3
Laure Bourgois <Laure.Bourgois@logilab.fr>
2009-03-06 11:15:57 +0100

    prepared version 0.3.1

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -4,7 +4,7 @@
 distname = 'cubicweb-expense'
 modname = distname.split('-', 1)[1]
 
-numversion = (0, 3, 0)
+numversion = (0, 3, 1)
 version = '.'.join(str(num) for num in numversion)
 
 license = 'LGPL'
