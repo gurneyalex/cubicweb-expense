eb8ab6b0aa4eaa1c37a4610cda6ef55d2ab0fd58
Sylvain Th?nault <sylvain.thenault@logilab.fr>
2009-09-17 16:26:00 +0200

    use 3.5 api

--- migration/postcreate.py
+++ migration/postcreate.py
@@ -1,12 +1,14 @@
 # expense workflow
-draft = add_state(_('draft'), 'Expense', initial=True)
-submitted = add_state(_('submitted'), 'Expense')
-accepted = add_state(_('accepted'),  'Expense')
-add_transition(_('accept'), 'Expense', (submitted,), accepted,
-               requiredgroups=('managers',))
-add_transition(_('submit'), 'Expense', (draft,), submitted)
+wf = add_workflow(_('expense workflow'), 'Expense')
+draft = wf.add_state(_('draft'), initial=True)
+submitted = wf.add_state(_('submitted'))
+accepted = wf.add_state(_('accepted'),  'Expense')
+wf.add_transition(_('accept'), submitted, accepted,
+                  requiredgroups=('managers',))
+wf.add_transition(_('submit'), draft, submitted)
 
 # refund workflow
-preparation = add_state(_('preparation'), 'Refund', initial=True)
-paid = add_state(_('paid'),  'Refund')
-pay = add_transition(_('pay'), 'Refund', (preparation,), paid)
+wf = add_workflow(_('refund workflow'), 'Refund')
+preparation = wf.add_state(_('preparation'), initial=True)
+paid = wf.add_state(_('paid'))
+pay = wf.add_transition(_('pay'), preparation, paid)
