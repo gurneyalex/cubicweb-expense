ad083bc8f29b0488a4757cf7ac9dcb4771d4234d
Sylvain Th?nault <sylvain.thenault@logilab.fr>
2010-03-23 17:22:17 +0100

    fix bad url

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -19,7 +19,7 @@ long_desc = '''\
 This cube provides to implement `expense tracking application`_
 by modeling `Expenses`, to ask for `Refund`.
 
-.. _`expense tracking application`: http://www.cubicweb.org/project/fresh
+.. _`expense tracking application`: http://www.cubicweb.org/project/cubicweb-fresh
 '''
 
 ftp = ''
